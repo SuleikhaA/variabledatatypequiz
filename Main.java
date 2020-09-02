f8bf8d7032ba0924b591a51cf688fd2378aadf54
SuleikhaA <31260562+SuleikhaA@users.noreply.github.com>
2020-09-02 20:14:23 +0000

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,33 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        //int
+        int firstGrade = 95;
+
+
+String str= "Lets have a great semester!";
+
+        //String
+        //float
+        float second_grade;
+        second_grade = (float) 98.3;
+
+
+
+        //boolean
+        boolean passing= true;
+        ///declare each type of variable
+        //give it a value
+        //output that value as part of a sentence
+        System.out.print("Your points in Java are: " + firstGrade + ". ");
+
+        System.out.print("You are passing your Java class: " + passing + ". ");
+
+        System.out.print("Your points in Intro to Cybersecurity are: " + second_grade + ". ");
+System.out.print(str);
+
+    }
+}
